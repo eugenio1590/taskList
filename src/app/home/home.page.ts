d49cb57b5942d2e14954de49e8d03465dc5f4806
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2026-04-24 22:10:52 -0500

    task list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,37 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  newTask: string = '';
+  tasks: Task[];
 
-  constructor() {}
+  constructor() {
+    this.tasks = [
+      { id: '1', title: 'This is an example of a task #1', completed: false },
+      { id: '2', title: 'This is an example of a task #2', completed: true }
+    ];
+  }
 
+  addTask() {
+    if (!this.newTask.trim()) return;
+
+    this.tasks.push({
+      id: Date.now().toString(),
+      title: this.newTask,
+      completed: false
+    });
+
+    this.newTask = '';
+  }
+
+  toggleTask(task: Task) {
+    task.completed = !task.completed;
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter(t => t.id !== task.id);
+  }
+
+  get remainingTasksCount() {
+    return this.tasks.filter(t => !t.completed).length;
+  }
 }
